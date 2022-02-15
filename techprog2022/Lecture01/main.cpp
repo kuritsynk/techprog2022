822bbb5fbabb7a7b609e6eab0c0f370f91802265
Konstantin Kuritsyn <kuritsynk@mail.ru>
2022-02-15 10:39:58 +0300

    2 Передача параметров в функции

--- techprog2022/Lecture01/main.cpp
+++ techprog2022/Lecture01/main.cpp
@@ -25,6 +25,9 @@ namespace MySpace {
 
 }
 
+using namespace MySpace::InnerSpace;
+using namespace std;
+
 // int min_int_int - внутреннее название функции
 int min(int a, int b) {
 	return a < b ? a : b;
@@ -35,11 +38,8 @@ double min(double a, double b) {
 	return a < b ? a : b;
 }
 
-void print(int x) {
-	cout << "x=" << x << endl;
-}
-
-void print(int x, bool negativeInBrackets) {
+// print_int_bool
+void print(int x = 0, bool negativeInBrackets = false) {
 	if (x < 0 && negativeInBrackets) {
 		cout << "x=(" << -x << ")" << endl;
 	}
@@ -48,8 +48,25 @@ void print(int x, bool negativeInBrackets) {
 	}
 }
 
-using namespace MySpace::InnerSpace;
-using namespace std;
+int foo(int x) {
+	// int x - входной параметр - локальные переменные функции 
+	x = x + 1;
+	return x;
+} // x уничтожается, т.к. время жизни закончилось
+
+int fooPtr(int* x) {
+	// int* x - локальная переменная
+	cout << "x=" << x << endl;
+	*x = *x + 1;  
+	return *x;
+} 
+
+int& fooRef(int& x) {
+	// int& x - локальная переменная - псевдоним для обращения через указатель
+	cout << "&x=" << &x << endl;
+	x = x + 1;
+	return x;
+}
 
 int main() {
 	std::cout << "Hello" << std::endl;
@@ -73,8 +90,41 @@ int main() {
 
 	min(3.14, 2.78); // min_double_double(3.14, 2.78)
 
+	// print_int_bool(2, false)
 	print(2); // x=2
-	print(-2, true); // x=(2)
+	bool negativeInBrackets = true;
+
+	// print_int_bool(-2, negativeInBrackets)
+	print(-2, negativeInBrackets); // x=(2)
+
+	print(); // print_int_bool(0, false)  x=0
+
+	// Передача параметров в функции
+
+	int k = 2;
+	// Передача по значению
+	int l = foo(k); // l = 3 k = 2 - k не изменился, foo::x - копия k
+	// int l = tmp (=return foo::x)
+
+	cout << "&k=" << &k << endl;
+	l = fooPtr(&k); // l = 3; k = 3; l = tmp (= return fooPtr::x) - копия &k
+
+	l = fooRef(k); // l = 4; k = 4; l = k // fooRef::x - псевдоним для k
+
+	{
+		int t = 5;
+		cout << "&t=" << &t << endl;
+	}
+	// cout << t << endl; // t - не существует
+	int s = 5;
+	cout << "&s=" << &s << endl;
+
+	int t = 5;
+	cout << "&t=" << &t << endl;
+
+	int z = ++k; // int& operator++(int& k) { k = k + 1; return k; }
+	int y = k++; // int operator++(int&k, int ignored) 
+	// { int t = k; k = k + 1; return t;  }
 
 
 	return 0;
